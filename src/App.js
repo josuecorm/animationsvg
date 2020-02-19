0605951cdc42f1798760f0b2767658dec83e0cc9
Josué Cornejo <josue.cornejo@wizeline.com>
2020-02-19 11:20:34 -0600

    Create SVGItem component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
+import SVGItem from "./components/SVGItem";
 
 function App() {
   return (
     <div className="App">
-      <p>Animation SVG</p>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              defaultValue="Instructions"
+            />
+          </div>
+          <div className="col">
+            <SVGItem />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
