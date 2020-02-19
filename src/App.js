663ef9786a237449862474ef896b42664bcb626f
Josué Cornejo <josue.cornejo@wizeline.com>
2020-02-19 17:22:11 -0600

    Add timeline controls

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ const tweens = [
     vars: {
       scaleX: 0.5
     }
+  },
+  {
+    targetId: "path264",
+    method: "from",
+    duration: 1,
+    vars: {
+      scaleX: 0.5
+    }
   }
 ];
 
@@ -17,11 +25,16 @@ function App() {
     <div className="App">
       <div className="container-fluid">
         <div className="row">
-          <nav className="col-md-2 d-none d-md-block bg-light sidebar">
+          <nav className="col-md-3 d-none d-md-block bg-light sidebar">
             <pre>{JSON.stringify(tweens, null, "  ")}</pre>
           </nav>
-          <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
-            <SVGItem url="/img/workflow.svg" tweens={tweens} />
+          <main role="main" className="col-md-9 ml-sm-auto px-4">
+            <SVGItem
+              url="/img/workflow.svg"
+              tweens={tweens}
+              controls={true}
+              autoplay={true}
+            />
           </main>
         </div>
       </div>
--- src/components/SVGItem/SVGItem.js
+++ src/components/SVGItem/SVGItem.js
@@ -12,8 +12,10 @@ class SVGFile extends Component {
     this.state = {
       error: null,
       loaded: false,
-      svgHTML: null
+      svgHTML: null,
+      currentStepIndex: 0
     };
+    this.timeline = null;
     this.container = React.createRef();
   }
 
@@ -28,22 +30,29 @@ class SVGFile extends Component {
   }
 
   createTimeline() {
-    const { tweens } = this.props;
+    const { tweens, controls, autoplay } = this.props;
     const { current } = this.container;
 
     if (!tweens.length) return;
 
-    const tl = gsap.timeline({ paused: true });
+    const tweenArray = [];
+
+    this.timeline = gsap.timeline({
+      paused: true,
+      ...(this.props.controls && { onUpdate: this.updateSlider })
+    });
 
     tweens.forEach(tween => {
       const { method, duration, targetId, vars } = tween;
       const element = current.querySelector(`#${targetId}`);
-      if (element) {
-        tl[method](element, { ...vars, duration: duration });
+      if (element && vars) {
+        tweenArray.push(gsap[method](element, { ...vars, duration: duration }));
       }
     });
 
-    tl.play();
+    this.timeline.add(tweenArray);
+
+    if (autoplay) this.timeline.play();
   }
 
   async loadFile() {
@@ -67,29 +76,92 @@ class SVGFile extends Component {
     }
   }
 
-  // handleClick(event) {
-  //   event.stopPropagation();
+  handleClick(event) {
+    event.stopPropagation();
 
-  //   if (event.target.tagName === "path") {
-  //     // console.log(event.target.className);
-  //     // event.target.className = "on";
-  //     event.target.setAttribute("class", "on");
-  //   }
-  // }
+    if (event.target.tagName === "path") {
+      // console.log(event.target.className);
+      // event.target.className = "on";
+      event.target.setAttribute("class", "on");
+    }
+  }
+
+  handleInputChange = event => {
+    const { value } = event.currentTarget;
+
+    if (this.timeline) {
+      this.timeline.progress(value).pause();
+    }
+
+    this.setState({ currentStepIndex: value });
+  };
+
+  updateSlider = () => {
+    if (this.timeline) {
+      this.setState({ currentStepIndex: this.timeline.progress() });
+    }
+  };
 
   render() {
-    const { loaded, error, svgHTML } = this.state;
+    const { controls } = this.props;
+    const { loaded, error, svgHTML, currentStepIndex } = this.state;
 
     if (!loaded) return null;
     if (error) return `Something went wrong: ${error.message}`;
 
     return (
-      <div
-        className="svg-container"
-        onClick={this.handleClick}
-        dangerouslySetInnerHTML={{ __html: svgHTML }}
-        ref={this.container}
-      />
+      <div className="wrapper">
+        <div
+          className="svg-container"
+          onClick={this.handleClick}
+          dangerouslySetInnerHTML={{ __html: svgHTML }}
+          ref={this.container}
+        />
+        {controls && (
+          <div className="controls">
+            <input
+              type="range"
+              className="custom-range"
+              id="customRange1"
+              min="0"
+              max="1"
+              step=".001"
+              value={currentStepIndex}
+              onChange={this.handleInputChange}
+            ></input>
+            <div className="btn-group" role="group" aria-label="Basic example">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={() => this.timeline.play()}
+              >
+                play
+              </button>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={() => this.timeline.pause()}
+              >
+                pause
+              </button>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={() => this.timeline.reverse()}
+              >
+                reverse
+              </button>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={() => this.timeline.restart()}
+              >
+                restart
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     );
   }
 }
@@ -98,5 +170,7 @@ export default SVGFile;
 
 SVGFile.propTypes = {
   url: PropTypes.string.isRequired,
-  tweens: PropTypes.array
+  tweens: PropTypes.array,
+  controls: PropTypes.bool,
+  autoplay: PropTypes.bool
 };
